de70aef42efeafd39e08bc570b2578b5e581fade
Tomas <tomascerdeyraimport@gmail.com>
2024-06-12 12:16:07 -0300

    first commit

---
+++ sistemaautogestion/src/main/java/com/grupoAutogestion/sistemaautogestion/SistemaAutogestionApplication.java
@@ -0,0 +1,13 @@
+package com.grupoAutogestion.sistemaautogestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAutogestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAutogestionApplication.class, args);
+	}
+
+}
---
+++ sistemaautogestion/src/test/java/com/grupoAutogestion/sistemaautogestion/SistemaAutogestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoAutogestion.sistemaautogestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAutogestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
