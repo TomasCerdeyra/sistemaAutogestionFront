8870f919c9d477d462ede4b983fc04228dcfa008
TobZhym <tobiasposse2395@gmail.com>
2024-07-01 17:24:26 -0300

    API GRUPO 1

--- sistemaautogestion/src/main/java/com/grupoAutogestion/sistemaautogestion/config/CustomAuthenticationProvider.java
+++ sistemaautogestion/src/main/java/com/grupoAutogestion/sistemaautogestion/config/CustomAuthenticationProvider.java
@@ -1,6 +1,7 @@
 package com.grupoAutogestion.sistemaautogestion.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.web.exchanges.HttpExchange.Principal;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -10,6 +11,10 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -24,36 +29,63 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
     @Autowired
     private RestTemplate restTemplate;
 
-    private static final String APIURL = "https://poo2024.unsada.edu.ar/sistema_login/login";
+    private static final String loginAPIURL = "http://localhost:4000/sistema_login/login";
+    private static final String authorizeAPIURL = "http://localhost:4000/sistema_login/authorize";
 
     @Override
     public Authentication authenticate(Authentication autentificacion) throws AuthenticationException {
         String username = autentificacion.getName();
         String password = autentificacion.getCredentials().toString();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        String jsonBody = String.format("{\"username\": \"%s\", \"password\": \"%s\"}", username, password);
-        HttpEntity<String> request = new HttpEntity<>(jsonBody, headers);
+        HttpHeaders headersLogin = new HttpHeaders();
+        headersLogin.setContentType(MediaType.APPLICATION_JSON);
+        String jsonBodyLogin = String.format("{\"username\": \"%s\", \"password\": \"%s\"}", username, password);
+        HttpEntity<String> requestLogin = new HttpEntity<>(jsonBodyLogin, headersLogin);
 
         try {
-            ResponseEntity<String> responseEntity = restTemplate.postForEntity(APIURL, request, String.class);
+            ResponseEntity<String> responseEntity = restTemplate.postForEntity(loginAPIURL, requestLogin, String.class);
 
             if (responseEntity.getStatusCode().is2xxSuccessful()) {
-                String respuesta = responseEntity.getBody();
-                List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
-
-                autentificacion = new UsernamePasswordAuthenticationToken(username, password, authorities);
-                ((UsernamePasswordAuthenticationToken) autentificacion).setDetails(respuesta);
+                String respuestaLogin = responseEntity.getBody();
                 //Testeo
-                // System.out.println(respuesta);
-                return autentificacion;
+                //System.out.println(respuestaLogin);
+
+                HttpHeaders headersAuthorize = new HttpHeaders();
+                headersAuthorize.setContentType(MediaType.APPLICATION_JSON);
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNode = objectMapper.readTree(respuestaLogin);
+
+                String jsonBodyAuthorize = String.format("{\"token\": \"%s\", \"systemId\": \"AUTOGESTION\"}", jsonNode.get("token").asText());
+                HttpEntity<String> requestAuthorize = new HttpEntity<>(jsonBodyAuthorize, headersAuthorize);
+
+                try{
+                    ResponseEntity<String> responseEntityAuthorize = restTemplate.postForEntity(authorizeAPIURL, requestAuthorize, String.class);
+                    if (responseEntityAuthorize.getStatusCode().is2xxSuccessful()) {
+                        List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+                        String respuestaAuthorize = responseEntityAuthorize.getBody();
+                        JsonNode jsonNode1 = objectMapper.readTree(respuestaAuthorize);
+
+                        //System.out.println(respuestaAuthorize);
+                        if (jsonNode1.get("authorized").asBoolean() == true){
+                            autentificacion = new UsernamePasswordAuthenticationToken(username, password, authorities);
+                            ((UsernamePasswordAuthenticationToken) autentificacion).setDetails(respuestaLogin);
+                            return autentificacion;
+                        }
+
+                    }
+                }
+                catch(Exception e){
+                    throw new BadCredentialsException("Username o Password invalidos");
+                }
+                
             } else {
                 throw new BadCredentialsException("Username o Password invalidos");
             }
         } catch (Exception e) {
             throw new BadCredentialsException("Error", e);
         }
+        return null;
     }
 
     @Override
